e29e00e73ebab8c35b897468217d1804288b132b
smsadat1 <214529054+smsadat1@users.noreply.github.com>
2026-09-21 14:20:50 +0600

    feat: Add marker on globe

--- client/globe.js
+++ client/globe.js
@@ -32,215 +32,254 @@ function latLonToVector3(lat, lon, radius) {
     );
 }
 
+
 export class Globe {
 
-	constructor() {
-	    this.timer = new THREE.Timer();
-	    this.timer.connect(document);
+    constructor() {
 
-    	this.camera = new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 100);
-    	this.camera.position.set(4.5, 2, 3);
+        this.timer = new THREE.Timer();
+        this.timer.connect(document);
 
-	    this.scene = new THREE.Scene();
-	    this.scene.background = new THREE.Color(0x000000);
-	}
+        this.camera = 
+			new THREE.PerspectiveCamera(25, window.innerWidth / window.innerHeight, 0.1, 100);
 
-	prepare_sun() {
-	    const sun = new THREE.DirectionalLight('#ffffff', 2);
-	    sun.position.set(0, 0, 3);
-	    this.scene.add(sun);
-	    this.sunOrientation =
-	        normalWorldGeometry.dot(normalize(sun.position)).toVar();
-	}
+        this.camera.position.set(4.5, 2, 3);
 
-	prepare_atmos() {
-    	
-		this.atmosphereDayColor = uniform(color('#18c52ce6'));
-    	this.atmosphereTwilightColor = uniform(color('#064d12'));
-		this.roughnessLow = uniform( 0.25 );
-		this.roughnessHigh = uniform( 0.35 );
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color(0x000000);
 
-		const textureLoader = new THREE.TextureLoader();
-		this.dayTexture = textureLoader.load( './assets/earth_day_4096.jpg' );
-		this.dayTexture.colorSpace = THREE.SRGBColorSpace;
-		this.dayTexture.anisotropy = 8;
+        this.globe = null;
+        this.sphereGeometry = null;
+        this.renderer = null;
+        this.controls = null;
+    }
 
-		this.nightTexture = textureLoader.load( './assets/earth_night_4096.jpg' );
-		this.nightTexture.colorSpace = THREE.SRGBColorSpace;
-		this.nightTexture.anisotropy = 8;
-		this.bumpRoughnessCloudsTexture = textureLoader.load( './assets/earth_bump_roughness_clouds_4096.jpg' );
-		this.bumpRoughnessCloudsTexture.anisotropy = 8;
+    start() {
 
-    	this.fresnel =
-    	    positionWorld
-    	        .sub(cameraPosition)
-    	        .normalize()
-    	        .dot(normalWorldGeometry)
-    	        .abs()
-    	        .oneMinus()
-    	        .toVar();
+        // Sun
+        const sun = new THREE.DirectionalLight('#d0dccc', 2);
+        sun.position.set(0, 0, 3);
+        this.scene.add(sun);
 
-    	this.atmosphereColor = mix(
-    	    this.atmosphereTwilightColor,
-    	    this.atmosphereDayColor,
-    	    this.sunOrientation.smoothstep(-0.25, 0.75)
-	    );
-	}
+        this.sunOrientation =
+            normalWorldGeometry.dot(normalize(sun.position)).toVar();
 
-	start_atmos() {
-    	const atmosphereMaterial =
-        	new THREE.MeshBasicNodeMaterial({side: THREE.BackSide, transparent: true});
 
-    	let alpha = this.fresnel.remap(0.73, 1, 1, 0).pow(3);
-    	alpha = alpha.mul(this.sunOrientation.smoothstep(-0.5, 1));
+        // Uniforms
+        this.atmosphereDayColor = uniform(color('#105b19'));
+        this.atmosphereTwilightColor = uniform(color('#064d12'));
+        this.roughnessLow = uniform(0.25);
+        this.roughnessHigh = uniform(0.35);
 
-    	atmosphereMaterial.outputNode = vec4(this.atmosphereColor, alpha);
 
-    	const atmosphere = new THREE.Mesh(this.sphereGeometry, atmosphereMaterial);
-    	atmosphere.scale.setScalar(1.04);
+        // Textures
+        const textureLoader = new THREE.TextureLoader();
 
-	    this.scene.add(atmosphere);
-	}
+        this.dayTexture = textureLoader.load('./assets/earth_day_4096.jpg');
+        this.dayTexture.colorSpace = THREE.SRGBColorSpace;
+        this.dayTexture.anisotropy = 8;
 
-	prepare_globe() {
-    	const globeMaterial = new THREE.MeshStandardNodeMaterial();
-    	const cloudsStrength = texture(this.bumpRoughnessCloudsTexture, uv()).b.smoothstep(0.2, 1);
+        this.nightTexture = textureLoader.load('./assets/earth_night_4096.jpg');
+        this.nightTexture.colorSpace = THREE.SRGBColorSpace;
+        this.nightTexture.anisotropy = 8;
 
-    	globeMaterial.colorNode = mix(texture(this.dayTexture), vec3(1), cloudsStrength.mul(1));
 
-    	const roughness = max(texture(this.bumpRoughnessCloudsTexture).g, step(0.01, cloudsStrength));
+        this.bumpRoughnessCloudsTexture =
+            textureLoader.load('./assets/earth_bump_roughness_clouds_4096.jpg');
+        this.bumpRoughnessCloudsTexture.anisotropy = 8;
 
-    	globeMaterial.roughnessNode = roughness.remap(0, 1, this.roughnessLow, this.roughnessHigh);
 
-   	 	const night = texture(this.nightTexture);
-   	 	const dayStrength = this.sunOrientation.smoothstep(-0.25, 0.5);
-    	const atmosphereDayStrength = this.sunOrientation.smoothstep(-0.5, 2);
+        // Fresnel
+        const viewDirection = positionWorld.sub(cameraPosition).normalize();
 
-    	const atmosphereMix =
-    	    atmosphereDayStrength.mul(this.fresnel.pow(2)).clamp(0, 1);
+        this.fresnel =
+            viewDirection.dot(normalWorldGeometry).abs().oneMinus().toVar();
 
-   	 	let finalOutput = mix(night.rgb, output.rgb, dayStrength);
-    	finalOutput = mix(finalOutput, this.atmosphereColor, atmosphereMix);
 
-    	globeMaterial.outputNode = vec4(finalOutput, output.a);
+        // Atmosphere color
+        this.atmosphereColor =
+            mix(
+                this.atmosphereTwilightColor,
+                this.atmosphereDayColor,
+                this.sunOrientation.smoothstep(-0.25, 0.75)
+            );
 
-    	const bumpElevation = max(texture(this.bumpRoughnessCloudsTexture).r, cloudsStrength);
 
-    	globeMaterial.normalNode = bumpMap(bumpElevation);
+        // Globe
+        const globeMaterial = new THREE.MeshStandardNodeMaterial();
+        const cloudsStrength =
+            texture(this.bumpRoughnessCloudsTexture, uv()).b.smoothstep(0.2, 1);
+        const cloudColor = vec3(0.15, 0.35, 0.18);
 
-    	this.sphereGeometry = new THREE.SphereGeometry(1, 64, 64);
-    	this.globe = new THREE.Mesh(this.sphereGeometry, globeMaterial);
-	    this.scene.add(this.globe);
-	}
 
-	start() {
-		this.prepare_sun();
-		this.prepare_atmos();
-		this.prepare_globe();
-		this.start_atmos();
-	}
+        globeMaterial.colorNode =
+            mix(texture(this.dayTexture), cloudColor, cloudsStrength.mul(0.1));
 
-	render() {
-	    this.renderer = new THREE.WebGPURenderer();
-    	this.renderer.setPixelRatio(window.devicePixelRatio);
-    	this.renderer.setSize(window.innerWidth, window.innerHeight);
-    	this.renderer.setAnimationLoop(() => this.animate());
+        const roughness =
+            max(texture(this.bumpRoughnessCloudsTexture).g, step(0.01, cloudsStrength));
 
-	    document.body.appendChild(this.renderer.domElement);
-	}
+        globeMaterial.roughnessNode =
+            roughness.remap(
+                0,
+                1,
+                this.roughnessLow,
+                this.roughnessHigh
+            );
 
-	orbitControl() {
-	    this.controls = new OrbitControls(
-	        this.camera,
-	        this.renderer.domElement
-	    );
 
-	    this.controls.enableDamping = true;
-	    this.controls.minDistance = 0.1;
-	    this.controls.maxDistance = 50;
-	}
+        const night = texture(this.nightTexture);
+        const dayStrength = this.sunOrientation.smoothstep(-0.25, 0.5);
+        const atmosphereDayStrength = this.sunOrientation.smoothstep(-0.5, 1);
+        const atmosphereMix = atmosphereDayStrength.mul(this.fresnel.pow(2)).clamp(0, 1);
 
-	addEvent() {
-    	window.addEventListener('resize', () => this.onWindowResize());
 
-    	gui
-    	    .addColor(
-    	        {
-    	            color: this.atmosphereDayColor.value.getHex(
-    	                THREE.SRGBColorSpace
-    	            )
-    	        },
-    	        'color'
-    	    )
-    	    .onChange((value) => {
-    	        this.atmosphereDayColor.value.set(value);
-    	    })
-    	    .name('atmosphereDayColor');
+        let finalOutput = mix(night.rgb, output.rgb, dayStrength);
+        finalOutput = mix(finalOutput, this.atmosphereColor, atmosphereMix);
 
-    	gui
-    	    .addColor(
-    	        {
-    	            color: this.atmosphereTwilightColor.value.getHex(
-    	                THREE.SRGBColorSpace
-    	            )
-    	        },
-    	        'color'
-    	    )
-    	    .onChange((value) => {
-    	        this.atmosphereTwilightColor.value.set(value);
-    	    })
-    	    .name('atmosphereTwilightColor');
+        globeMaterial.outputNode = vec4(finalOutput, output.a);
 
-    	gui
-    	    .add(this.roughnessLow, 'value', 0, 1, 0.001)
-    	    .name('roughnessLow');
 
-    	gui
-    	    .add(this.roughnessHigh, 'value', 0, 1, 0.001)
-    	    .name('roughnessHigh');
-	}	
+        const bumpElevation =
+            max(texture(this.bumpRoughnessCloudsTexture).r, cloudsStrength.mul(0.1));
+        globeMaterial.normalNode = bumpMap(bumpElevation);
 
-	onWindowResize() {
-    	this.camera.aspect = window.innerWidth / window.innerHeight;
-    	this.camera.updateProjectionMatrix();
-	    this.renderer.setSize(window.innerWidth, window.innerHeight);
-	}
 
-	async animate() {
-	    this.timer.update();
+        this.sphereGeometry = new THREE.SphereGeometry(1, 64, 64);
+        this.globe = new THREE.Mesh(this.sphereGeometry, globeMaterial);
+        this.scene.add(this.globe);
 
-	    const delta = this.timer.getDelta();
+        // Atmosphere
+        const atmosphereMaterial =
+            new THREE.MeshBasicNodeMaterial({
+                side: THREE.BackSide,
+                transparent: true
+            });
 
-	    this.globe.rotation.y += delta * 0.025;
-	    this.controls.update();
-	    this.renderer.render(this.scene, this.camera);
-	}
 
-	addMarker(lon, lat, status) {
-    	let color;
+        let alpha =
+            this.fresnel
+                .remap(0.73, 1, 1, 0)
+                .pow(3);
 
-    	switch (status) {
-    	    case "ACTIVE":
-    	        color = 0x18c52b;
-    	        break;
 
-    	    case "IDLE":
-    	        color = 0x666666;
-    	        break;
+        alpha =
+            alpha.mul(
+                this.sunOrientation.smoothstep(-0.5, 1)
+            );
 
-    	    case "OFFLINE":
-    	        color = 0xd52b2b;
-    	        break;
 
-    	    default:
-    	        throw new Error(`Unknown marker status: ${status}`);
-    	}
+        atmosphereMaterial.outputNode =
+            vec4(
+                this.atmosphereColor,
+                alpha
+            );
 
-    	const geometry = new THREE.SphereGeometry(0.025, 12, 12);
-    	const material = new THREE.MeshBasicMaterial({ color });
-    	const marker = new THREE.Mesh(geometry, material);
-    	marker.position.copy(latLonToVector3(lat, lon, 1.02));
-    	this.scene.add(marker);
-    	return marker;
-	}
+
+        const atmosphere =
+            new THREE.Mesh(
+                this.sphereGeometry,
+                atmosphereMaterial
+            );
+
+
+        atmosphere.scale.setScalar(1.04);
+
+        this.scene.add(atmosphere);
+
+    }
+
+
+    render() {
+
+        this.renderer =
+            new THREE.WebGPURenderer();
+
+        this.renderer.setPixelRatio(
+            window.devicePixelRatio
+        );
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+        this.renderer.setAnimationLoop(
+            () => this.animate()
+        );
+
+        document.body.appendChild(
+            this.renderer.domElement
+        );
+
+    }
+
+
+    orbitControl() {
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enableDamping = true;
+        this.controls.minDistance = 0.1;
+        this.controls.maxDistance = 50;
+    }
+
+
+    addEvent() {
+        window.addEventListener('resize', () => this.onWindowResize());
+    }
+
+
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+
+    addMarker(lon, lat, status) {
+
+        let markerColor;
+
+        switch (status) {
+            case "ACTIVE":
+                markerColor = 0x18c52b;
+                break;
+            case "IDLE":
+                markerColor = 0xd0dccc;
+                break;
+            case "OFFLINE":
+                markerColor = 0xd52b2b;
+                break;
+            default:
+                throw new Error(`Unknown marker status: ${status}`);
+        }
+
+		const group = new THREE.Group();
+
+		// Core
+		const coreGeometry = new THREE.SphereGeometry(0.009, 16, 16);
+		const coreMaterial = new THREE.MeshBasicMaterial({color: markerColor});
+		const core = new THREE.Mesh(coreGeometry, coreMaterial);
+		group.add(core);
+
+
+		// Ring
+		const ringGeometry = new THREE.RingGeometry(0.015, 0.011, 32);
+		const ringMaterial = new THREE.MeshBasicMaterial({
+		    color: markerColor, transparent: true, opacity: 0.7, side: THREE.DoubleSide
+		});
+		const ring = new THREE.Mesh(ringGeometry, ringMaterial);
+		group.add(ring);
+        
+        group.position.copy(latLonToVector3(lat, lon, 1.06));
+        this.globe.add(group);        
+		return group;
+    }
+
+
+    animate() {
+        this.timer.update();
+        const delta = this.timer.getDelta();
+        this.globe.rotation.y += delta * 0.025;
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
 }
--- client/main.js
+++ client/main.js
@@ -6,4 +6,10 @@ globe.start();
 globe.render();
 globe.orbitControl();
 globe.addEvent();
-globe.addMarker(1.3043, 103.4343, "ACTIVE")
+
+globe.addMarker(103.8198, 1.3521, "ACTIVE");    // Singapore
+globe.addMarker(72.8774, 19.0761, "OFFLINE");   // Mumbai
+globe.addMarker(8.6821, 50.1109, "ACTIVE");    // Frankfurt
+globe.addMarker(-78.65, 37.43, "IDLE");         // Virginia
+globe.addMarker(55.296249, 25.276987, "ACTIVE") // Dubai
+globe.addMarker(139.6917, 35.6895, "IDLE")      // Tokyo
